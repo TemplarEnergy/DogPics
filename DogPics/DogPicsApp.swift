9af7fe0afe05ef8795a2eba0a4ec285b9a59d41b
Tom Radford <tom@radfords-web.com>
2023-02-11 19:19:48 -0400

    Initial Commit

---
+++ DogPics/DogPicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DogPicsApp.swift
+//  DogPics
+//
+//  Created by Thomas Radford on 11/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct DogPicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
